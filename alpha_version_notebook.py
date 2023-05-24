9f08e1f7a1e6aae6a4041803bc6e912de22dbfe5
evinyes <1606019@uab.cat>
2023-05-24 16:04:48 +0000

    jejejeje

--- alpha_version_notebook.py
+++ alpha_version_notebook.py
@@ -9,9 +9,10 @@ Original file is located at
 
 from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
 from keras.layers import Activation, Dense, Dropout, Flatten
-from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import array_to_img, img_to_array, load_img
 from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
 from skimage.io import imsave
 import numpy as np
@@ -20,7 +21,7 @@ import random
 import tensorflow as tf
 
 # Get images
-image = img_to_array(load_img('woman.jpg'))
+image = img_to_array(load_img('swim.jpg'))
 image = np.array(image, dtype=float)
 
 X = rgb2lab(1.0/255*image)[:,:,0]
