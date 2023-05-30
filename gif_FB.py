3fcde0a41d02c2fec605abf06dd85726bcd9431f
MarinaSu <1527602@uab.cat>
2023-05-30 09:51:20 +0000

    fitxer nou

--- gif_FB.py
+++ gif_FB.py
@@ -7,19 +7,18 @@ Original file is located at
     https://colab.research.google.com/drive/169QqsfCcGUdHqy_AwOerpRm2zXCTCeWB
 """
 
-from tensorflow.keras.layers import Conv2D, UpSampling2D, Input
-from tensorflow.keras.models import Model
+from keras.layers import Conv2D, UpSampling2D, InputLayer
+from keras.models import Sequential
 from skimage.transform import resize
 from skimage.color import rgb2lab, lab2rgb
 from skimage.io import imread, imsave
-from skimage import color, transform
-from PIL import Image
 import numpy as np
+from PIL import Image
 import imageio
+
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import ArtistAnimation
-
 
 
 # Función para aplicar el proceso de coloración a un cuadro
@@ -27,13 +26,13 @@ def colorize_frame(frame):
     # Preprocesar el cuadro de imagen (por ejemplo, convertir RGB a LAB)
     gray_frame = rgb2lab(frame)[:, :, 0]
     gray_frame = resize(gray_frame, (400, 400))
-    X = gray_frame.reshape(1, 400, 400, 1)
+    X = gray_frame.reshape(400, 400, 1)
 
     # Apilar el cuadro en una entrada de 3 canales (RGB)
     # X = np.repeat(X, 3, axis=2)
 
     # Obtener la predicción del modelo para el cuadro
-    output = model.predict(X)
+    output = model.predict(np.expand_dims(X, axis=0))
     output *= 128
 
     # Generar la imagen final en color
@@ -43,38 +42,31 @@ def colorize_frame(frame):
 
     # Convertir la imagen de LAB a RGB
     colored_frame = lab2rgb(colored_frame)
-    colored_frame = (colored_frame * 255).astype(np.uint8)
 
     return colored_frame
 
 
 # Building the neural network
-input_shape = (400, 400, 1)
-inputs = Input(shape=input_shape)
+model = Sequential()
+model.add(InputLayer(input_shape=(None, None, 1)))
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same')) 
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(inputs)
-x = Conv2D(8, (3, 3), activation='relu', padding='same', strides=2)(x)
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-x = Conv2D(16, (3, 3), activation='relu', padding='same', strides=2)(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same', strides=2)(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same', strides=2)(x)
-
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-outputs = Conv2D(8, (3, 3), activation='tanh', padding='same')(x)
-
-model = Model(inputs=inputs, outputs=outputs)
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 
 
 # Abrir el archivo GIF
-gif = Image.open('Vho.gif')
+gif = Image.open('espiral.gif')
 frames = []
 
 for frame in range(gif.n_frames):
@@ -97,6 +89,7 @@ for frame in range(gif.n_frames):
     frames.append(colored_frame)
 
 
+
 # Crear la figura y el eje
 fig, ax = plt.subplots()
 ax.axis('off')
@@ -110,11 +103,13 @@ for frame in frames:
 # Crear la animación
 ani = ArtistAnimation(fig, artists, interval=gif.info['duration'], blit=True)
 
+
 # Guardar los cuadros generados 
-imageio.mimsave('Vho_02.gif', frames)
+imageio.mimsave('animacion.gif', frames)
 
 # Abrir y mostrar el GIF guardado
-saved_gif = Image.open('Vho_02.gif')
+saved_gif = Image.open('animacion.gif')
+# saved_gif.show()
 
 # Abrir y mostrar el GIF guardado utilizando plt.imshow()
 plt.imshow(saved_gif)
